c2289a25496bf6fe790f78474a36f00302c06cb4
Roberto Corcuera <robecorcuera@gmail.com>
2019-05-07 14:01:14 +0200

    primer envío

---
+++ js/codigo.js
@@ -0,0 +1,8 @@
+// Éste es un comentario de una línea
+/* Mira por donde este es un comentario de bloque.
+Sigue siendo un conmentario de bloque.
+Te lo crees, ¿verdad? */
+let miNombre = prompt("Introduce tu nombre") {
+  while miNombre == "Peñ"
+  alert ("Sí, te conozco, eres" + miNombre + ".");
+}
