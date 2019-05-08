a01ba5c2cbab33f7e10ba0d82b5ca52f80140616
Roberto Corcuera <robecorcuera@gmail.com>
2019-05-08 09:33:22 +0200

    Ëste vale

--- js/codigo.js
+++ js/codigo.js
@@ -2,7 +2,12 @@
 /* Mira por donde este es un comentario de bloque.
 Sigue siendo un conmentario de bloque.
 Te lo crees, ¿verdad? */
-let miNombre = prompt("Introduce tu nombre") {
-  while miNombre == "Peñ"
-  alert ("Sí, te conozco, eres" + miNombre + ".");
-}
+
+let miNombre = prompt("Introduce tu nombre");
+  if (miNombre == "Pepito Grillo") {
+    alert("Bienvenido de nuevo, Pepito Grillo... y no veas lo contento que estoy de que este código salga aceptable.");
+  } else {
+    alert ("Sí, te conozco, eres " + miNombre + "... pero no eres nuestro amiguete del alma.");
+       window.alert(texto);
+    }
+// texto = "Sí, te conozco, eres " + miNombre + ".";
